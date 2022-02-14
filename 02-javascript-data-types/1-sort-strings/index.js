58a196fea2daa1c9461eb3cc0610616fba07b7ed
i.kukarkin <ilya1196@yandex.ru>
2022-02-14 23:05:52 +0500

    fix sort strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -16,53 +16,5 @@ export function sortStrings(arr, param = 'asc') {
 }
 
 const compareWords = (a, b) => {
-  const aChar = a.charCodeAt(0);
-  const bChar = b.charCodeAt(0);
-    
-  // Оба из Русского алфавита
-  if (aChar > 1000 && bChar > 1000) {
-
-    // Обработка 'Ё' и 'ё' для aChar
-    if (aChar === 1025 || aChar === 1105) {
-        
-      // Возвращаем 'е' вместо 'ё' и 'Е' вместо 'Ё' с приведением Регистра у bChar
-      return returnFunction(1045, bChar > 1071 ? bChar - 32 : bChar);
-    }
-    
-    // Обработка 'Ё' и 'ё' для bChar
-    if (bChar === 1025 || bChar === 1105) {
-
-      // Возвращаем 'е' вместо 'ё' и 'Е' вместо 'Ё' с приведением Регистра у aChar
-      return returnFunction(aChar > 1071 ? aChar - 32 : aChar, 1045);
-    }
-
-    // Возвращаем с приведением Регистра
-    return returnFunction(aChar > 1071 ? aChar - 31 : aChar, bChar > 1071 ? bChar - 31 : bChar);
-  }
-
-  // Оба из Английского алфавита
-  if (aChar < 1000 && bChar < 1000) {
-
-    // Возвращаем с приведением Регистра
-    return returnFunction(aChar > 96 ? aChar - 31 : aChar, bChar > 96 ? bChar - 31 : bChar);
-  }
-
-  // Один из Русского алфавита
-  if (aChar > 1000 || bChar > 1000) {
-
-    // Возвращаем без регистра, но первым Английский алфавит
-    return returnReverseFunction(aChar, bChar);
-  }
-};
-
-const returnFunction = (a, b) => {
-  if (a > b) {return 1;}
-  if (a == b) {return 0;}
-  if (a < b) {return -1;}
-};
-
-const returnReverseFunction = (a, b) => {
-  if (a > b) {return -1;}
-  if (a == b) {return 0;}
-  if (a < b) {return 1;}
+  return a.localeCompare(b, ['ru', 'en'], {caseFirst: "upper"});
 };
